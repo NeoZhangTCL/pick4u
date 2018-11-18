e9dbe595180763bd4e8633c5e1b20a8fafa86ab2
NeoZhang <ztuochao@uwo.ca>
2018-11-18 17:31:41 -0500

    config mocha test

--- test/random_pick.test.js
+++ test/random_pick.test.js
@@ -1,8 +1,12 @@
-import "../src/random_pick";
+var rp = require('../lib/random_pick');
+var assert = require('chai').assert;
 
-describe("random pick test", function() {
+describe("random pick test", () => {
 
-    var dict = {
+    const epsilion = 0.001 ;
+    const limit = 10000000;
+    const dict_sum = 10; // it has to be the sum of dict values
+    const dict = {
         "a": 2,
         "b": 3,
         "c": 5
@@ -12,17 +16,15 @@ describe("random pick test", function() {
         "b": 0,
         "c": 0
     };
-    const epsilion = 0.0001 ;
-    const limit = 100000000;
       
-    it("gap between result percentage and actual percentage should be smaller than epsilion", function() {
+    it("gap between result percentage and actual percentage should be smaller than epsilion", () => {
         for (var i = 0; i < limit; i++) {
             var dict2 = Object.assign({}, dict);
-            var r = random_pick(dict2);
+            var r = rp(dict2);
             stats[r]++;
         }
-        assert.isTrue(Math.abs(1/stats["a"] - 0.2) < epsilion, "the percentage of a should be about 0.2");
-        assert.isTrue(Math.abs(1/stats["b"] - 0.3) < epsilion, "the percentage of b should be about 0.3");
-        assert.isTrue(Math.abs(1/stats["c"] - 0.5) < epsilion, "the percentage of c should be about 0.5");
-    });
+        assert.isTrue(Math.abs(stats["a"]/limit - dict["a"]/dict_sum) < epsilion, "the percentage of a should be about 0.2");
+        assert.isTrue(Math.abs(stats["b"]/limit - dict["b"]/dict_sum) < epsilion, "the percentage of b should be about 0.3");
+        assert.isTrue(Math.abs(stats["c"]/limit - dict["c"]/dict_sum) < epsilion, "the percentage of c should be about 0.5");
+    }).timeout(10000);;
 });
